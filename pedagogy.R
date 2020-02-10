401820d97738589e181858daa2827c0160fff3cd
SMarkH <smarkhami1995@gmail.com>
2020-02-10 08:07:30 -0700

    initial commit

--- pedagogy.R
+++ pedagogy.R
@@ -12,26 +12,40 @@ ped <- read.table("https://mheaton.byu.edu/docs/files/Stat469/Topics/1%20-%20Ind
                   sep = " ")
 
 # Exploratory Data Analysis
+# scater plot of number of students and final score
 ggplot(ped, aes(x=NStudents, y=Final)) + 
   xlab("Number of Students") +
   ylab("Final Grade") +
   geom_point()
 
+# scater plot of exam 1 score and final score 
 ggplot(ped, aes(x=Exam1, y=Final)) + 
   xlab("Exam1 Score") +
   ylab("Final Grade") +
   geom_point()
 
+# scater plot of exam 2 and final score
 ggplot(ped, aes(x=Exam2, y=Final)) + 
   xlab("Exam2 Score") +
   ylab("Final Grade") +
   geom_point()
 
+# scatter plot of exam3 and final score
 ggplot(ped, aes(x=Exam3, y=Final))  + 
-  xlab("Number of Students") +
+  xlab("Exam3 Score") +
   ylab("Final Grade") +
   geom_point()
 
-ggplot(ped, aes(x=HW, y=Final)) + geom_point()
-ggplot(ped, aes(x=Quiz, y=Final)) + geom_point()
+# scatter plot of homework score and final score
+ggplot(ped, aes(x=HW, y=Final))  + 
+  xlab("Homework Score") +
+  ylab("Final Grade") +
+  geom_point()
 
+# scatter plot of quiz scores and final scores
+ggplot(ped, aes(x=Quiz, y=Final))  + 
+  xlab("Quiz Score") +
+  ylab("Final Grade") +
+  geom_point()
+
+# fit hetero skedastic and linear and transform quizes check assumptions on all of them
