33ce810437450e40b58ae59b2728e027b8a4f9f3
smarkh <smarkhami1995@gmail.com>
2020-02-10 08:35:26 -0700

    initial commit

--- pedagogy.R
+++ pedagogy.R
@@ -4,6 +4,13 @@
 
 # Libraries
 library(ggplot2)
+library(GGally)
+library(car)
+library(MASS)
+library(lmtest)
+library(multcomp)
+library(magrittr)
+library(nlme)
 
 
 # Grab data
@@ -49,3 +56,38 @@ ggplot(ped, aes(x=Quiz, y=Final))  +
   geom_point()
 
 # fit hetero skedastic and linear and transform quizes check assumptions on all of them
+lm_mod <- lm(Final~. , data = ped)
+summary(lm_mod)
+
+# check assumptions
+# Construct added variable plots and assess if the linearity assumption is OK for this data.
+avPlots(lm_mod)
+# the linearity assumption seems correct because the plot appears to be linear
+
+# Construct a histogram of the standardized residuals and run a KS-test to see if the normality 
+# assumption is OK for this data.
+ggplot()+geom_histogram(mapping=aes(x=stdres(lm_mod)))
+# This data appears to be normally distributed
+
+# Draw a scatterplot of the fitted values vs. standardized residuals and run a BP-test to see if 
+# the equal variance assumption is OK for this data
+plot(stdres(lm_mod) ~ fitted.values(lm_mod), 
+     ylab = 'standardized residuals',
+     xlab = 'fitted values')
+abline(0, 0, col='red')
+bptest(lm_mod)
+# the plot appears to show equal variance because the residuals are not equally varied around zero, the bp test 
+# also confirms this as the pvalue is greater than .05
+
+##############################################################
+hs_mod <- gls(model = Final ~ ., data = ped, method = "ML", 
+              weights=varExp(form=~Quiz)) 
+summary(hs_mod)
+
+
+#betas
+hs_mod$coefficients
+# Theta
+coef(hs_mod$modelStruct, unconstrained=FALSE)
+# sigma
+hs_mod$sigma
